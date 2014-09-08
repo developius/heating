317457262f37cd99706d27db706a3c16a0dff484
Finnian Anderson <finnian@fxapi.co.uk>
2014-09-08 21:15:34 +0000

    Update comments

--- test_temp.py
+++ test_temp.py
@@ -1,19 +1,16 @@
 from pyRF24 import pyRF24
-import time, mode
+import time
+import mode # sets up pipes
 radio = pyRF24("/dev/spidev0.0", 8000000, 18, retries = (15, 15), channel = 76, dynamicPayloads = True, autoAck = True)
 
 while True:
-#	mode.send()
+#	mode.send() calls radio.stopListening()
 	inputed = input("Node: ")
 	while not radio.write(str(inputed + "temp")):
 		print("Failed")
 	print("Sent")
-	mode.recv()
+	mode.recv() # calls radio.startListening()
 	time.sleep(0.25)
 	if radio.available():
 		payload = radio.read(radio.getDynamicPayloadSize())
-		if payload is not None: print("Got: %s" % payload)
-	
-"""for i in range(0,101):
-	if radio.write(str(i)): print(i, "sent")
-	time.sleep(0.25)"""
+		if payload is not None: print("Got: %s" % payload) # gets gibberish
