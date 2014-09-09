784d99aaf4d1a6065cd10d25bbdc5622aa8e43a8
Finnian Anderson <finnian@fxapi.co.uk>
2014-09-09 15:18:00 +0000

    Fix pipe numbering

--- mode.py
+++ mode.py
@@ -1,7 +1,7 @@
 from pyRF24 import pyRF24
 pipes = [0xF0F0F0F0E1, 0xF0F0F0F0E2]
 radio = pyRF24("/dev/spidev0.0", 8000000, 18, retries = (15, 15), channel = 76, dynamicPayloads = True, autoAck = True)
-radio.openWritingPipe(pipes[1])
+radio.openWritingPipe(pipes[0])
 radio.openReadingPipe(1, pipes[1])
 
 def recv():
