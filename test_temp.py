18095c9d19aa302383dc9a344886c6063f5c3f7c
Finnian Anderson <finnian@fxapi.co.uk>
2014-09-09 16:00:36 +0000

    Fix pipe setup

--- test_temp.py
+++ test_temp.py
@@ -1,6 +1,9 @@
 from pyRF24 import pyRF24
 import time, binascii
 radio = pyRF24("/dev/spidev0.0", 8000000, 18, retries = (15, 15), channel = 76, dynamicPayloads = True, autoAck = True)
+pipes = [0xF0F0F0F0E1, 0xF0F0F0F0E2]
+radio.openWritingPipe(pipes[0]) 	
+radio.openReadingPipe(1, pipes[1])
 
 def recv():
 	radio.startListening()
