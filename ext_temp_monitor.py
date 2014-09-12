530e559f8bcb2a62914e898e1ed55db22096726b
Finnian Anderson <finnian@fxapi.co.uk>
2014-09-12 16:17:14 +0100

    Monitors external temp

---
+++ ext_temp_monitor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+from pyRF24 import pyRF24
+import time, binascii, time, os, sys, gspread, datetime, json, mysql.connector
+from urllib import request
+
+credentials = []
+
+for line in open("credentials.txt", "r"):
+	credentials.append(line.split("\n")[0])
+
+db = mysql.connector.connect(user=credentials[1], password=credentials[2], host=credentials[0], database=credentials[3], autocommit = True)
+cur = db.cursor()
+
+gc = gspread.login(credentials[4], credentials[5])
+ws = gc.open("Heating Data").sheet1
+
+def ext_temp():
+	req = request.urlopen('http://api.openweathermap.org/data/2.5/find?q=laxfield&units=metric')
+	encoding = req.headers.get_content_charset()
+	obj = json.loads(req.read().decode(encoding))
+	return obj['list'][0]['main']['temp']
+
+while True:
+	cur.execute("INSERT INTO heating.ext_temp_log VALUES ('%s', '%.2f')" % (time.strftime("%d-%m-%Y %H:%M:%S"), ext_temp()))
+	time.sleep(60) # * 60
