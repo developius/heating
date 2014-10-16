506291d9ad6c5333035ff833da963628f0245b26
Finnian Anderson <finnian@fxapi.co.uk>
2014-10-16 15:44:33 +0100

    Fix MySQL server timeout

--- monitor.py
+++ monitor.py
@@ -7,28 +7,7 @@ credentials = []
 
 for line in open("credentials.txt", "r"):
 	credentials.append(line.split("\n")[0])
-
 connection = {"sql":False,"gs":False}
-
-print("Connecting to MySQL database... ", end="")
-try:
-	db = mysql.connector.connect(user=credentials[1], password=credentials[2], host=credentials[0], database=credentials[3], autocommit = True)
-	cur = db.cursor()
-	connection['sql'] = True
-	print("Connected")
-except:
-	connection['sql'] = False
-	print("Failed")
-
-print("Connecting to GS... ", end="")
-try:
-	gc = gspread.login(credentials[4], credentials[5])
-	ws = gc.open("Heating Data").sheet1
-	connection['gs'] = True
-	print("Connected")
-except:
-	connection['gs'] = False
-	print("Failed")
 
 radio = pyRF24("/dev/spidev0.0", 8000000, 25, retries = (15, 15), channel = 76, dynamicPayloads = True, autoAck = True)
 radio.setDataRate(2)
@@ -50,10 +29,10 @@ for node in nodes:
 	last_status["%i" % node] = None
 
 def ext_temp():
-        req = request.urlopen('http://api.openweathermap.org/data/2.5/find?q=laxfield&units=metric')
-        encoding = req.headers.get_content_charset()
-        obj = json.loads(req.read().decode(encoding))
-        return obj['list'][0]['main']['temp']
+	req = request.urlopen('http://api.openweathermap.org/data/2.5/find?q=laxfield&units=metric')
+	encoding = req.headers.get_content_charset()
+	obj = json.loads(req.read().decode(encoding))
+	return obj['list'][0]['main']['temp']
 
 def recv():
 	radio.startListening()
@@ -64,28 +43,41 @@ def send():
 	time.sleep(0.25)
 
 while True:
-	for node in nodes:
-		changed = False
-		send()
-		timeout = False
-		started_waiting_at = time.time()
-		while not radio.write(str(str(node) + "temp")) and not timeout:
-			if ((time.time() - started_waiting_at) > 5): timeout = True
-		if timeout: print("Node %i down" % node); break
-#			cur.execute("UPDATE node_data SET Status='%s' WHERE Node='%i';" % ("off", node))
-#			cur.execute("UPDATE node_data SET Status='%s' WHERE Node='%i';" % ("on", node)) # is supposed to detect if node is up but if
-														# you turn off the node in the db, it turns
-														# it back off
-		hour = datetime.datetime.now().hour
-		print("Hour: %i" % hour)
-		recv()
-		timeout = False
-		started_waiting_at = time.time()
-		while not radio.available() and not timeout:
-			if ((time.time() - started_waiting_at) > 5): timeout = True
-		if timeout: break
-		else:
-			if connection['sql']:
+	try:
+		db = mysql.connector.connect(user=credentials[1], password=credentials[2], host=credentials[0], database=credentials[3], autocommit = True)
+		cur = db.cursor()
+		connection['sql'] = True
+	except:
+		connection['sql'] = False
+	try:
+		gc = gspread.login(credentials[4], credentials[5])
+		ws = gc.open("Heating Data").sheet1
+		connection['gs'] = True
+	except:
+		connection['gs'] = False
+
+	if connection['sql']:
+		for node in nodes:
+			changed = False
+			send()
+			timeout = False
+			started_waiting_at = time.time()
+			while not radio.write(str(str(node) + "temp")) and not timeout:
+				if ((time.time() - started_waiting_at) > 5): timeout = True
+			if timeout: print("Node %i down" % node); break
+	#			cur.execute("UPDATE node_data SET Status='%s' WHERE Node='%i';" % ("off", node))
+	#			cur.execute("UPDATE node_data SET Status='%s' WHERE Node='%i';" % ("on", node)) # is supposed to detect if node is up but if
+															# you turn off the node in the db, it turns
+															# it back off
+			hour = datetime.datetime.now().hour
+			print("Hour: %i" % hour)
+			recv()
+			timeout = False
+			started_waiting_at = time.time()
+			while not radio.available() and not timeout:
+				if ((time.time() - started_waiting_at) > 5): timeout = True
+			if timeout: break
+			else:
 				payload = radio.read(radio.getDynamicPayloadSize())
 				temp = binascii.hexlify(payload)
 				temp = temp.decode('ascii')
@@ -112,24 +104,26 @@ while True:
 					print("	| Change with device status: %s" % Status)
 					last_status['%i' % node] = Status
 					changed = True
-		if changed == True:
-			send()
-			time.sleep(0.25)
-			timeout = False
-			print("	| Sending " + str(node) + str(last_threshold["%i" % node][hour]) + str(last_status["%i" % node]) + "...", end="")
-			started_waiting_at = time.time()
-			while not radio.write(str(node) + str(last_threshold["%i" % node][hour]) + str(last_status["%i" % node])) and not timeout:
-				if ((time.time() - started_waiting_at) > 5): timeout = True
-			if timeout:
-				print("	failed")
-				last_threshold["%i" % node][hour] = None # --| next time round it's still different so that we retry to update the node
-				last_status["%i" % node] = None		 # --| ''
-			else:
-				print(" sent")
-		time.sleep(10)
-	if connection['gs']:
-		try:
-			ws.append_row([time.strftime("%Y-%m-%d %H:%M:%S"), node_temp["0"], node_temp["1"], node_temp["2"], ext_temp()])
-		except:
-			pass
-#			print("Insert into GS failed")
+			cur.close()
+			db.close()
+			if changed == True:
+				send()
+				time.sleep(0.25)
+				timeout = False
+				print("	| Sending " + str(node) + str(last_threshold["%i" % node][hour]) + str(last_status["%i" % node]) + "...", end="")
+				started_waiting_at = time.time()
+				while not radio.write(str(node) + str(last_threshold["%i" % node][hour]) + str(last_status["%i" % node])) and not timeout:
+					if ((time.time() - started_waiting_at) > 5): timeout = True
+				if timeout:
+					print("	failed")
+					last_threshold["%i" % node][hour] = None # --| next time round it's still different so that we retry to update the node
+					last_status["%i" % node] = None		 # --| ''
+				else:
+					print(" sent")
+			time.sleep(10)
+		if connection['gs']:
+			try:
+				ws.append_row([time.strftime("%Y-%m-%d %H:%M:%S"), node_temp["0"], node_temp["1"], node_temp["2"], ext_temp()])
+			except:
+				pass
+	#			print("Insert into GS failed")
